46a9cba15477e144cfe927e9e875d774c0418b80
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-05-03 17:45:01 -0700

    Player team initialization setup

--- Assets/_Project/_Scripts/Player/PlayerNetwork.cs
+++ Assets/_Project/_Scripts/Player/PlayerNetwork.cs
@@ -1,16 +1,23 @@
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private RectTransform _bluePanel;
+    [SerializeField] private RectTransform _redPanel;
     [SerializeField] private GameObject _scavengerBlueUnit;
     [SerializeField] private GameObject _scavengerRedUnit;
     [SerializeField] private GameObject _blueTower;
     [SerializeField] private GameObject _redTower;
     [SerializeField] private LayerMask _towerNodeLayer;
+    [SerializeField] private int _currentScrapBank = 250;
+    [SerializeField] private int _passiveScrapRate = 10;
 
     private DefaultControls _defaultControls;
+    private TextMeshProUGUI _scrapBankText;
+    private TextMeshProUGUI _scrapRateText;
 
     private void Awake()
     {
@@ -35,20 +42,37 @@ public class PlayerNetwork : NetworkBehaviour
         _defaultControls.Debug.SpawnOpponentUnit.started += SpawnOpponentUnit;
         _defaultControls.Player.Click.started += OnClick;
 
-        if (OwnerClientId == 0)
-            InitializeAsBlueTeam();
-        else
-            InitializeAsRedTeam();
+        if (IsOwner)
+        {
+            if (OwnerClientId == 0)
+                InitializeAsBlueTeam();
+            else
+                InitializeAsRedTeam();
+
+            UpdateUI();
+        }
     }
 
     private void InitializeAsBlueTeam()
     {
-        Debug.Log("InitializeAsBlueTeam Callback");
+        _bluePanel.gameObject.SetActive(true);
+        _redPanel.gameObject.SetActive(false);
+        _scrapBankText = _bluePanel.GetChild(1).GetComponent<TextMeshProUGUI>();
+        _scrapRateText = _bluePanel.GetChild(2).GetComponent<TextMeshProUGUI>();
     }
 
     private void InitializeAsRedTeam()
     {
-        Debug.Log("InitializeAsRedTeam Callback");
+        _redPanel.gameObject.SetActive(true);
+        _bluePanel.gameObject.SetActive(false);
+        _scrapBankText = _redPanel.GetChild(1).GetComponent<TextMeshProUGUI>();
+        _scrapRateText = _redPanel.GetChild(2).GetComponent<TextMeshProUGUI>();
+    }
+
+    private void UpdateUI()
+    {
+        _scrapBankText.text = $"Scrap Bank: {_currentScrapBank}";
+        _scrapRateText.text = $"Scrap Rate: {_passiveScrapRate}";
     }
 
     private void SpawnUnit(InputAction.CallbackContext context)
