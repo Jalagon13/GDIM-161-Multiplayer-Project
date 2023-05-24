a21e69841b64884b3b3a39659031e59bab988799
alegottu <33403572+alegottu@users.noreply.github.com>
2023-05-23 19:32:38 -0700

    added working resource nodes to main scene

--- Assets/_Project/_Scripts/Entities/Nodes/ResourceNode.cs
+++ Assets/_Project/_Scripts/Entities/Nodes/ResourceNode.cs
@@ -7,18 +7,13 @@ public class ResourceNode : MonoBehaviour
     public static event Action<bool> OnResourceNodeObtained;
     public static event Action<bool> OnResourceNodeLost;
 
+    [SerializeField] private SpriteRenderer render;
     [SerializeField] private Color unobtained;
     [SerializeField] private Color redTeamObtained;
     [SerializeField] private Color blueTeamObtained;
 
     // Assumes that the map only allows for each team to have one tower near each resource node; confirm with design team
     private Tower[] _towers = new Tower[2];
-    private SpriteRenderer render;
-
-    private void Awake()
-    {
-        render = GetComponent<SpriteRenderer>();
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -52,6 +47,7 @@ public class ResourceNode : MonoBehaviour
                 if (_towers[i].IsRed() != isRed)
                 {
                     _towers[0] = _towers[i];
+                    _towers[1].OnDestroyed -= OnTowerDestroyedEventHandler;
                     _towers[1] = null;
                     OnResourceNodeObtained?.Invoke(!isRed);
                     render.color = isRed ? blueTeamObtained : redTeamObtained;
@@ -62,6 +58,7 @@ public class ResourceNode : MonoBehaviour
         else
         {
             OnResourceNodeLost?.Invoke(isRed);
+            _towers[0].OnDestroyed -= OnTowerDestroyedEventHandler;
             _towers[0] = null;
             render.color = unobtained;
         }
